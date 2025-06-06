4cb254f9b73f083a7dde0fe67ea878a18e7a4a8d
PedroTrudes <pedro.jtrudes@gmail.com>
2025-06-05 23:38:43 -0300

    :lipstick: feat: add Tailwind by CDN project

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Book</title>
+      <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
        <h1>Iniciando projeto com PHP do Web Book</h1> 
